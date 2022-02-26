7cea54b7cd6e4cae61c2334f1159e84177b832b3
Riex <ralexander@live.se>
2022-02-26 16:53:34 +0100

    added more movement func, updated quickbrake

--- main.cpp
+++ main.cpp
@@ -13,23 +13,44 @@
 #include "movement.h"
 
 //---------Defining pins----------------
-#define echopin_FL 50 // Forward Left ultrasonic sensor
-#define trigpin_FL 52
-#define echopin_FR 47 // Forward Right ultrasonic sensor
-#define trigpin_FR 49
-#define echopin_BL 51 // Back Left ultrasonic sensor
-#define trigpin_BL 53
-#define echopin_BR 46 // Back Right ultrasonic sensor
-#define trigpin_BR 48
+#define echopin_FL 40 // Forward Left sensor
+#define trigpin_FL 41
+#define echopin_FR 42 // Forward Right sensor
+#define trigpin_FR 43
+#define echopin_BL 44 // Back Left
+#define trigpin_BL 45
+#define echopin_BR 46 // Back Right
+#define trigpin_BR 47
 
-#define FWDpin_FL 9 // FWD Forward left
-#define BWDpin_FL 8 // BWD -||-
-#define FWDpin_FR 7 // FWD Forward Right
-#define BWDpin_FR 6 // BWD -||-
-#define FWDpin_BL 5 // FWD Backward Left
-#define BWDpin_BL 4 // BWD -||-
-#define FWDpin_BR 3 // FWD Backward Right
-#define BWDpin_BR 2 // BWD -||-
+#define STATE 50      // STATE PIN HC05
+
+#define FWDpin_FL 8 // FWD Forward left
+#define BWDpin_FL 9 // BWD
+#define FWDpin_FR 4 // FWD Forward Right
+#define BWDpin_FR 5 // BWD
+#define FWDpin_BL 6 // FWD Backward LefB
+#define BWDpin_BL 7 // BWD
+#define FWDpin_BR 10 // FWD Backward Right
+#define BWDpin_BR 11 // BWD
+
+
+#define SENSORA_F1 A4   // Sensor ramp forward
+#define SENSORA_F2 A5
+#define SENSORA_F3 A6
+#define SENSORA_F4 A7
+#define SENSORA_F5 A0
+#define SENSORA_F6 A1
+#define SENSORA_F7 A2
+#define SENSORA_F8 A3
+
+#define SENSORA_B1 A8   // sensor ramp backward
+#define SENSORA_B2 A12
+#define SENSORA_B3 A9
+#define SENSORA_B4 A13
+#define SENSORA_B5 A10
+#define SENSORA_B6 A14
+#define SENSORA_B7 A11
+#define SENSORA_B8 A15
 
 //--------Changeable variables-----------
 int tolerance = 10;                 // Allowed error before the translation corection algorithm is implemented
@@ -74,6 +95,24 @@ void setup()
   pinMode(BWDpin_BL, OUTPUT);
   pinMode(FWDpin_BR, OUTPUT);
   pinMode(BWDpin_BR, OUTPUT);
+
+  pinMode(SENSORA_F1,INPUT);
+  pinMode(SENSORA_F2,INPUT);
+  pinMode(SENSORA_F3,INPUT);
+  pinMode(SENSORA_F4,INPUT);
+  pinMode(SENSORA_F5,INPUT);
+  pinMode(SENSORA_F6,INPUT);
+  pinMode(SENSORA_F7,INPUT);
+  pinMode(SENSORA_F8,INPUT);
+  
+  pinMode(SENSORA_B1,INPUT);
+  pinMode(SENSORA_B2,INPUT);
+  pinMode(SENSORA_B3,INPUT);
+  pinMode(SENSORA_B4,INPUT);
+  pinMode(SENSORA_B5,INPUT);
+  pinMode(SENSORA_B6,INPUT);
+  pinMode(SENSORA_B7,INPUT);
+  pinMode(SENSORA_B8,INPUT);
 
   Serial.begin(9600);                                            // // Serial Communication is starting with 9600 of baudrate speed
   Serial.println("Ultrasonic Sensor HC-SR04 Test, translation"); // print some text in Serial Monitor
