367ae1b95f3f1b2696f452fb03e48b4660dbf22d
Riex <ralexander@live.se>
2022-03-10 13:06:12 +0100

    Added startup melody

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include "correction.h"
 #include "SoftwareSerial.h"
 #include "movement.h"
+#include "audio.h"
 
 //---------Defining pins----------------
 #define echopin_FL 40 // Forward Left sensor
@@ -25,6 +26,7 @@
 #define trigpin_BR 47
 
 #define STATE 50      // STATE PIN HC05
+#define buzzer_pin 30 // pin for audio buzzer
 
 #define FWDpin_FL 8 // FWD Forward left
 #define BWDpin_FL 9 // BWD
@@ -60,6 +62,7 @@ int tolerance_angle = 20;           // Allowed error before the rotational corre
 float angle = 18.33;                // Angle of the sensors from the vehicle
 int start_pwm = 100;                      // Base PWM before modifiers. from 0 to 255
 int correction_pwm = 60;
+int startup_sound = 1;              // if 1 = sing if 0 dont sing
 
 // --------Defining variables------------
 long travelTime_FL;
@@ -131,8 +134,13 @@ void setup()
   pinMode(SENSORA_B7,INPUT);
   pinMode(SENSORA_B8,INPUT);
 
+  pinMode(buzzer_pin, OUTPUT);
   Serial.begin(9600);                                            // // Serial Communication is starting with 9600 of baudrate speed
   Serial.println("Ultrasonic Sensor HC-SR04 Test, translation"); // print some text in Serial Monitor
+
+  
+  sing(startup_sound);
+
 }
 
 //-----------Main loop-------------------------------------
@@ -148,61 +156,61 @@ void loop()
 
 //   translational_correction(real_distance_FL, real_distance_BL, real_distance_FR, real_distance_BR, tolerance, start_pwm);
 
-  last_real_distance_FL = real_distance_FL;
-  last_real_distance_FR = real_distance_FR;
-  last_real_distance_BL = real_distance_BL;
-  last_real_distance_BR = real_distance_BR;
+//   last_real_distance_FL = real_distance_FL;
+//   last_real_distance_FR = real_distance_FR;
+//   last_real_distance_BL = real_distance_BL;
+//   last_real_distance_BR = real_distance_BR;
   
-  real_distance_FL = real_distance(ultraSensor(trigpin_FL, echopin_FL), angle);
-  real_distance_FR = real_distance(ultraSensor(trigpin_FR, echopin_FR), angle);
-  real_distance_BL = real_distance(ultraSensor(trigpin_BL, echopin_BL), angle);
-  real_distance_BR = real_distance(ultraSensor(trigpin_BR, echopin_BR), angle);
+//   real_distance_FL = real_distance(ultraSensor(trigpin_FL, echopin_FL), angle);
+//   real_distance_FR = real_distance(ultraSensor(trigpin_FR, echopin_FR), angle);
+//   real_distance_BL = real_distance(ultraSensor(trigpin_BL, echopin_BL), angle);
+//   real_distance_BR = real_distance(ultraSensor(trigpin_BR, echopin_BR), angle);
   
-    // rotate_delay = -0.0004*pow(start_pwm, 3) + 0.2537*pow(start_pwm,2) - 53.176*(start_pwm) + 4288.3;
-    z = (start_pwm - 175) / 47.6;
-    rotate_delay = -45.1 * pow(z,3) + 77.5 * pow(z,2) - 133 * pow(z,1) + 510;
+//     // rotate_delay = -0.0004*pow(start_pwm, 3) + 0.2537*pow(start_pwm,2) - 53.176*(start_pwm) + 4288.3;
+//     z = (start_pwm - 175) / 47.6;
+//     rotate_delay = -45.1 * pow(z,3) + 77.5 * pow(z,2) - 133 * pow(z,1) + 510;
 
-    // Serial.println(rotate_delay);
-    // rotate_centered_clkw(start_pwm);
-    // delay(rotate_delay);
-    // rotate_stop();
-    // delay(10000);
+//     // Serial.println(rotate_delay);
+//     // rotate_centered_clkw(start_pwm);
+//     // delay(rotate_delay);
+//     // rotate_stop();
+//     // delay(10000);
     
  
 
  
-  if(!nope && !biggus) {translate_FWD(start_pwm);}
-  if((!biggus && abs(last_real_distance_FL-real_distance_FL) > 200) || (!biggus && abs(last_real_distance_FR-real_distance_FR) > 200 ))
-  {
-      delay(300);
-    translate_stop();
-    nope = true;
-    delay(1500);
-  }
-    if(!turned && nope && !biggus)
-  {
-    rotate_centered_clkw(start_pwm);
-    delay(rotate_delay);
-    rotate_stop();
-    delay(1000);
-    turned = true;
-  }
-  if(!goagain && nope && turned && !biggus)
-  {
-    translate_FWD(start_pwm);
-    delay(2000);
-    translate_stop();
+//   if(!nope && !biggus) {translate_FWD(start_pwm);}
+//   if((!biggus && abs(last_real_distance_FL-real_distance_FL) > 200) || (!biggus && abs(last_real_distance_FR-real_distance_FR) > 200 ))
+//   {
+//       delay(300);
+//     translate_stop();
+//     nope = true;
+//     delay(1500);
+//   }
+//     if(!turned && nope && !biggus)
+//   {
+//     rotate_centered_clkw(start_pwm);
+//     delay(rotate_delay);
+//     rotate_stop();
+//     delay(1000);
+//     turned = true;
+//   }
+//   if(!goagain && nope && turned && !biggus)
+//   {
+//     translate_FWD(start_pwm);
+//     delay(2000);
+//     translate_stop();
         
-    goagain = true;
-    biggus = true;
-    cp_variabel = true;
-  }
-  if (cp_variabel) {
-      rotational_correction(real_distance_FL, real_distance_BL, real_distance_FR, real_distance_BR, tolerance_angle, correction_pwm);
+//     goagain = true;
+//     biggus = true;
+//     cp_variabel = true;
+//   }
+//   if (cp_variabel) {
+//       rotational_correction(real_distance_FL, real_distance_BL, real_distance_FR, real_distance_BR, tolerance_angle, correction_pwm);
 
-      translational_correction(real_distance_FL, real_distance_BL, real_distance_FR, real_distance_BR, tolerance, correction_pwm);
+//       translational_correction(real_distance_FL, real_distance_BL, real_distance_FR, real_distance_BR, tolerance, correction_pwm);
 
-  }
+//   }
 
 
 }
--- movement.cpp
+++ movement.cpp
@@ -1,6 +1,11 @@
 // Projectgroup 1
 // Bachelors-project in Electronics Design engineering
+// 2022
+
 // Alexander Riex ED4
+// Henrik Nilsson ED3
+// Konrad Råström ED3
+// Petter Danev ED5
 
 //-------------Libraries---------------
 #include "movement.h"
